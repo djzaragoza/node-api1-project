4a9861c3e2d6b8880bcdb6be64a20ad31601c76f
unknown <dj.zaragoza@icloud.com>
2020-03-27 17:07:16 -0700

    created src folder and index.js file to start building web api

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const http = require('http'); // built in node.js module to handle http traffic
+
+const hostname = '127.0.0.1'; // local computer where the server is running 
+const port = 3000; // a port we will use to watch for traffic
+
